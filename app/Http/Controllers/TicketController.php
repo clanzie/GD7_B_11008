691bed9e20bb87df680ff5be7166418dd567401f
clanzie <ryanpratama4121@gmail.com>
2023-10-17 00:54:08 +0700

    first commit guided 7

---
+++ app/Http/Controllers/TicketController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ticket;
+use Illuminate\Http\Request;
+
+class TicketController extends Controller
+{
+    public function index(){
+        $ticket = Ticket::get();
+
+        return view('index', compact('ticket'));
+    }
+}
