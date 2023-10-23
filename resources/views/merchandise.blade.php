8aefd754ed18883749365cfc0ae3ac47fdf804f1
clanzie <ryanpratama4121@gmail.com>
2023-10-23 19:51:39 +0700

    UGD7_Final

--- resources/views/merchandise.blade.php
+++ resources/views/merchandise.blade.php
@@ -67,24 +67,20 @@
         margin-right: 100px;
     }
 
-    /* width */
     ::-webkit-scrollbar {
         width: 10px;
     }
 
-    /* Track */
     ::-webkit-scrollbar-track {
         box-shadow: inset 0 0 5px grey;
         border-radius: 10px;
     }
 
-    /* Handle */
     ::-webkit-scrollbar-thumb {
         background: #D2B873;
         border-radius: 10px;
     }
 
-    /* Handle on hover */
     ::-webkit-scrollbar-thumb:hover {
         background: #D2B873;
     }
